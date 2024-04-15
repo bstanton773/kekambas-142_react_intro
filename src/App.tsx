6511e0ded8cabcaaad093145b2fff344cb13cba8
Brian <bstanton773@gmail.com>
2024-04-15 14:38:19 -0500

    Update App component with conditional rendering and rendering an array

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
 
 export default function App(){
-    return <h1>Hello World</h1>
+    const firstName: string = 'Brian';
+    const lastName: string = 'Stanton';
+    const isLoggedIn: boolean = true;
+
+    const posts: {id:number, title:string}[] = [
+        {id: 1, title: 'Happy Monday'},
+        {id: 2, title: 'React Rules!'},
+        {id: 3, title: 'Spring has Sprung'}
+    ]
+
+    return (
+        <>
+            <h1>Hello World</h1>
+            <h2>{isLoggedIn ? `Welcome Back ${firstName} ${lastName}` : 'Please Log In or Sign Up'}</h2>
+            {posts.map( p => <h4 key={p.id}>{p.title}</h4> )}
+        </>
+    )
 }
