7dcc72fccb98646f9eb5ae89e624efd51a697fc9
Brian <bstanton773@gmail.com>
2024-04-16 16:37:51 -0500

    Add Post Form to add new posts to the posts array

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -4,7 +4,8 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import PostCard from '../components/PostCard';
-import { PostType } from '../types';
+import PostForm from '../components/PostForm';
+import { PostFormDataType, PostType } from '../types';
 
 
 type Sorting = {
@@ -22,6 +23,7 @@ type HomeProps = {
 
 export default function Home({isLoggedIn, handleClick}: HomeProps) {
 
+    const [showForm, setShowForm] = useState(false);
     const [posts, setPosts] = useState<PostType[]>([
             {
                 author: {
@@ -72,13 +74,20 @@ export default function Home({isLoggedIn, handleClick}: HomeProps) {
         setSearchTerm(e.target.value);
     }
 
+    const addNewPost = (newPostData: PostFormDataType) => {
+        const author = {id: 1, firstName: 'Brian', lastName: 'Stanton', email: 'brians@ct.com', username:'brians', dateCreated: "Tue, 14 Apr 2024 16:58:44 GMT"};
+        const newPost: PostType = {...newPostData, id:posts.length+1, dateCreated:new Date().toString(), author};
+        setPosts([...posts, newPost]);
+        setShowForm(false);
+    }
+
     return (
         <>
             <h1>Hello World</h1>
                 <Button variant='primary' onClick={handleClick}>Click Me!</Button>
                 <h2>{isLoggedIn ? `Welcome Back` : 'Please Log In or Sign Up'}</h2>
                 <Row>
-                    <Col xs={12} md={8}>
+                    <Col xs={12} md={6}>
                         <Form.Control value={searchTerm} placeholder='Search Posts' onChange={handleInputChange} />
                     </Col>
                     <Col>
@@ -90,7 +99,11 @@ export default function Home({isLoggedIn, handleClick}: HomeProps) {
                             <option value="titleDesc">Sort By Title DESC</option>
                         </Form.Select>
                     </Col>
+                    <Col>
+                        <Button className='w-100' variant='success' onClick={() => setShowForm(!showForm)}>{showForm ? 'Hide Form' : 'Add Post+'}</Button>
+                    </Col>
                 </Row>
+                { showForm && <PostForm addNewPost={addNewPost} /> }
                 {posts.filter(p => p.title.toLowerCase().includes(searchTerm.toLowerCase())).map( p => <PostCard key={p.id} post={p} /> )}
         </>
     )
